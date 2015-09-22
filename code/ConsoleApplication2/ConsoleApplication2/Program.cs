b3ae066e6c746f7ebf83c0a670002113f36732dc
IceLedor <danijar.gusev@gmail.com>
2015-09-22 13:20:24 +0300

    jfsjsfj

--- code/ConsoleApplication2/ConsoleApplication2/Program.cs
+++ code/ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApplication2
         static void Main(string[] args)
         {
             int a = 1;
-            Console.ReadKey();
+            Console.ReadKey();sasqs
         }
     }
 }
